cd108369174f8f661c28916de8a6fc215acff508
fncaio <sdfncaio6@gmail.com>
2024-10-26 16:18:40 -0300

    Exercício Entradas de dados Python Proz TC38

--- ExercicioProzTC38Calculadora.py
+++ ExercicioProzTC38Calculadora.py
@@ -1,14 +1,32 @@
-def calculadora (num1, num2, operacao):
-    if(operacao == 1):
-        return num1 + num2
+def calculadora (pnum, snum, operacao):
+   
+    if (operacao == 1):
+        return pnum + snum
     elif (operacao == 2):
-        return num1 - num2
+        return pnum - snum
     elif (operacao == 3):
-        return num1 * num2
+        return pnum * snum
     elif (operacao == 4):
-        return num1 / num2
+        return pnum / snum
     else:
         return 0
     
-resultado = calculadora (4, 7, 2)
-print(resultado)
+executar = True
+while (executar == True):
+        print("Escolha o tipo de operação que deseja fazer, sendo 1 = soma, 2 = subitração 3 = multiplicação e 4 = divisão ou 0 para sair")
+        operacao = int(input())
+        if(operacao< 0) or (operacao > 4):
+             print("Essa não é uma opção valida")
+        elif(operacao == 0):
+             executar = False
+        else:
+            print("Escolha o primeiro número")
+            pnum = int(input())
+            print("Escolha o segundo número")
+            snum = int(input())
+        
+       
+        
+
+            resultado = calculadora(pnum, snum, operacao)    
+            print("o Resultado é " + resultado)
